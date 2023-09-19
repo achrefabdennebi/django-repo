f5c376a77303c3577e1d74c436878292c9edec47
achrefabdennebi <achref.abdennebi@gmail.com>
2023-09-19 23:26:49 +0200

    feat: init django project

---
+++ core/project/settings/custom.py
@@ -0,0 +1,5 @@
+"""
+Settings specific to this application only (no django or third part settings)
+"""
+
+IN_DOCKER = False
---
+++ core/project/settings/templates/settings.dev.py
@@ -0,0 +1,2 @@
+DEBUG = False
+SECRET_KEY = 'django-insecure-5%q#5xd%f(0-dxfk7c-gf@e(n4p+whz)d!i!8ry04&7r^7=6w('
