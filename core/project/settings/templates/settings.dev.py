23dce29698c572884eec186f7601aef2c31113a0
achrefabdennebi <achref.abdennebi@gmail.com>
2023-09-29 08:53:28 +0200

    feat: add logging

--- core/project/settings/templates/settings.dev.py
+++ core/project/settings/templates/settings.dev.py
@@ -1,2 +1,10 @@
-DEBUG = False
+DEBUG = True
 SECRET_KEY = 'django-insecure-5%q#5xd%f(0-dxfk7c-gf@e(n4p+whz)d!i!8ry04&7r^7=6w('
+
+LOGGING['formatters']['colored'] = {  # type: ignore
+    '()': 'colorlog.ColoredFormatter',
+    'format': '%(log_color)s%(asctime)s %(levelname)s %(name)s %(bold_white)s%(message)s',
+}
+LOGGING['loggers']['core']['level'] = 'DEBUG'  # type: ignore
+LOGGING['handlers']['console']['level'] = 'DEBUG'  # type: ignore
+LOGGING['handlers']['console']['formatter'] = 'colored'  # type: ignore
